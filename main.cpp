2e95c637449f1400b1b92df52884e6f10f7ddf4d
C4ptainHook <boykostudyinkpi@gmail.com>
2023-06-14 20:58:59 +0300

    Fix

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include "LZW_Archiver.hpp"
-#include "File_service.hpp"
+#include <functional>
 
 int main(int argc, char* argv[]) {
-   std::string action = fs::validateArguments(argc, argv);
-   if(action=="--compress")
+    std::string cmp = "--compress";
+    std::string dcmp = "--decompress";
+
+    std::function action = [=] (int argc, char* argv[]){
+        if(argc<3){
+            throw runtime_error("Too few arguments");
+        }
+        if(argv[1]!=cmp&&argv[1]!=dcmp)
+        {throw runtime_error("Program does not support "+string(argv[1])+"argument");}
+        return argv[1];
+    };
+
+   if(action(argc,argv)==cmp)
    {
        for (int i = 2; i < argc; ++i) {
            lzw::Compress(argv[i]);
        }
    }
-   else if(action=="--decompress")
+   else if(action(argc,argv)==dcmp)
    {
        for (int i = 2; i < argc; ++i) {
            lzw::Decompress(argv[i]);
